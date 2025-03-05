9cbb4e5fb599c9e60b26f18c782fbc0a40948378
X0fei <pitonvano05@gmail.com>
2025-03-05 16:11:47 +0300

    Login

---
+++ TechService/RequestListWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TechService;
+
+public partial class RequestListWindow : Window
+{
+    public RequestListWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TechService/Utils/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechService.Context;
+using TechService.Models;
+
+namespace TechService.Utils
+{
+    public static class Context
+    {
+        public static User21Context DbContext { get; set; } = new();
+        public static List<User> Users = new List<User>(DbContext.Users.ToList());
+    }
+}
