b6d9af0766c61ad97a7b17de5ab5ea2ba46f7e8c
X0fei <pitonvano05@gmail.com>
2025-03-05 15:57:02 +0300

    Database reconected

--- TechService/Models/ReportMaterial.cs
+++ TechService/Models/ReportMaterial.cs
@@ -8,4 +8,8 @@ public partial class ReportMaterial
     public int ReportId { get; set; }
 
     public int MaterialId { get; set; }
+
+    public virtual Material Material { get; set; } = null!;
+
+    public virtual Report Report { get; set; } = null!;
 }
